8035e454b89b44c260ed78eeec6dca264962b32c
rafaelrds <crafards@gmail.com>
2018-01-03 12:28:19 -0300

    Making the project deployable at Heroku

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ Hello: 'World!' });
+app.get('/api/hello', (req, res) => {
+  res.send({ Hello: 'World...' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
-console.log(`Server listening on port ${PORT}\n`);
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function() {
+  console.log(`Server listening on port ${PORT}\n`);
+});
