c86ca1a63f7c60f24b0f6b80efc988a1351999bc
rafaelrds <crafards@gmail.com>
2018-01-05 18:11:53 -0300

    Creating client-side testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,27 @@
+// libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// actions
+import { getProducts } from './actions/product_actions';
+import reducers from './reducers/index';
+
+// component
+import { App } from './components/App';
+
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(reducers, middleware);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , div);
+
 });
